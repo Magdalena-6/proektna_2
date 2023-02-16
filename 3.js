ca3b1b72cb264747aff9906ecd2d47da53ac49b3
Magdalena <115716656+Magdalena-6@users.noreply.github.com>
2023-02-17 00:35:24 +0100

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,44 @@
+
+
+function lajk1() {
+    
+  const totalClicks = document.getElementById("br-lajk1");
+  const sumValue = parseInt(totalClicks.innerText) + 1;
+  totalClicks.innerText = sumValue;
+}
+
+function lajk2() {
+  
+  const totalClicks = document.getElementById("br-lajk2");
+  const sumValue = parseInt(totalClicks.innerText) + 1;
+  totalClicks.innerText = sumValue;
+}
+
+function lajk3() {
+  
+  const totalClicks = document.getElementById("br-lajk3");
+  const sumValue = parseInt(totalClicks.innerText) + 1;
+  totalClicks.innerText = sumValue;
+}
+
+function lajk4() {
+  
+  const totalClicks = document.getElementById("br-lajk4");
+  const sumValue = parseInt(totalClicks.innerText) + 1;
+  totalClicks.innerText = sumValue;
+}
+
+function lajk5() {
+  
+  const totalClicks = document.getElementById("br-lajk5");
+  const sumValue = parseInt(totalClicks.innerText) + 1;
+  totalClicks.innerText = sumValue;
+}
+
+function lajk6() {
+  
+  const totalClicks = document.getElementById("br-lajk6");
+  const sumValue = parseInt(totalClicks.innerText) + 1;
+  totalClicks.innerText = sumValue;
+}
+    
